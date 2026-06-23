cf5e79297ddd312d181ec7affe165dfbc66e0df9
Bishal Bhusal <167902074+the-bsx@users.noreply.github.com>
2026-06-23 22:23:39 +0545

    Change SameSite cookie attribute to 'none'

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -188,7 +188,7 @@ const loginUser = asyncHandler(async (req, res) => {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
   return res.status(200).json(
